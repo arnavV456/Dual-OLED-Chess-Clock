b1a7c3d3f0b66c2a2ae2d95652554c52d6dc598f
Arnav Verma <147163265+arnavV456@users.noreply.github.com>
2025-03-30 11:42:30 +0530

    Add pin definitions according to schematic

---
+++ Chess-clock.ino/Chess-clock.ino.ino
@@ -0,0 +1,18 @@
+#define CLK1                A4
+#define DATA1               A5
+#define CLK2                A2
+#define DATA2               A3
+#define INCREMENT_BTN_1     2
+#define DECREMENT_BTN_1     3
+#define INCREMENT_BTN_2     4
+#define DECREMENT_BTN_2     5
+#define MODE_BTN            6
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+}
