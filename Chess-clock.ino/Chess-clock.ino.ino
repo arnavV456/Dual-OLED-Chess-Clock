fbe8f1357a7b831f749fce0b7cfb663252549af1
Arnav Verma <147163265+arnavV456@users.noreply.github.com>
2025-04-02 22:43:39 +0530

    complete functionality without player buttons

--- Chess-clock.ino/Chess-clock.ino.ino
+++ Chess-clock.ino/Chess-clock.ino.ino
@@ -23,8 +23,10 @@ int hours_1 = 0;
 int mins_1  = 0;
 int secs_1  = 0;
 
-int increment_1 = 0;
-int increment_2 = 0; 
+int increment_1_min = 0;
+int increment_1_sec = 0;
+int increment_2_min = 0;
+int increment_2_sec = 0; 
 
 int hours_2 = 0;
 int mins_2  = 0;
@@ -32,6 +34,8 @@ int secs_2  = 0;
 
 char buffer1[30];
 char buffer2[30];
+char buffer3[30];
+char buffer4[30];
 
 int initial_state= 0;
 int mode_counter =0 ;
@@ -50,6 +54,7 @@ void setup() {
   // Setting the initial state of the main button so we can start the game when this state changes 
   initial_state = set_initial_state();
 
+
   // Setting pin modes for various buttons 
   pinMode(PLAYER_1,INPUT);
   pinMode(PLAYER_2,INPUT);
@@ -59,12 +64,15 @@ void setup() {
   pinMode(LED,OUTPUT);
 
   display1_on();
+  display2_on();
 
   set_display1_time();
+ 
+ 
 }
 
 void loop() {
- 
+timer1_on();
 
 }
 
@@ -121,7 +129,7 @@ void set_display1_time(void){
       }
     }
   }
-
+   // SET seconds for timer 1
    if(mode_counter==3)
   {
     if(digitalRead(INCREMENT_BTN)==HIGH)
@@ -139,10 +147,166 @@ void set_display1_time(void){
       }
     }
   }
+
+  // Ability to set increment of minutes 
+
+  if(mode_counter==4)
+  {
+    display1_increment_on();
+    if(digitalRead(INCREMENT_BTN)==HIGH)
+    {
+      delay(500);
+      increment_1_min++;
+      display1_increment_on();
+    }
+    if(digitalRead(DECREMENT_BTN)==HIGH)
+    {
+      if(increment_1_min != 0){
+      delay(500);
+      increment_1_min--;
+      display1_increment_on();
+      }
+    }
+  } 
+
+  // set increment seconds 
+  if(mode_counter==5)
+  {
+    display1_increment_on();
+    if(digitalRead(INCREMENT_BTN)==HIGH)
+    {
+      delay(500);
+      increment_1_sec++;
+      display1_increment_on();
+    }
+    if(digitalRead(DECREMENT_BTN)==HIGH)
+    {
+      if(increment_1_sec != 0){
+      delay(500);
+      increment_1_sec--;
+      display1_increment_on();
+      }
+    }
+  } 
+  
+
+  if(mode_counter==6)
+  {
+    display1_on();
+    display2_on();
+   if(digitalRead(INCREMENT_BTN)==HIGH)
+    {
+      delay(500);
+      hours_2++;
+      display2_on();
+    }
+    if(digitalRead(DECREMENT_BTN)==HIGH)
+    {
+      if(hours_2 != 0){
+      delay(500);
+      hours_2--;
+      display2_on();
+      }
+    }
+  } 
+  if(mode_counter==7)
+  {
+    display2_on();
+   if(digitalRead(INCREMENT_BTN)==HIGH)
+    {
+      delay(500);
+      mins_2++;
+      display2_on();
+    }
+    if(digitalRead(DECREMENT_BTN)==HIGH)
+    {
+      if(mins_2 != 0){
+      delay(500);
+      mins_2--;
+      display2_on();
+      }
+    }
+  } 
+
+   if(mode_counter==8)
+  {
+    display2_on();
+   if(digitalRead(INCREMENT_BTN)==HIGH)
+    {
+      delay(500);
+      secs_2++;
+      display2_on();
+    }
+    if(digitalRead(DECREMENT_BTN)==HIGH)
+    {
+      if(secs_2 != 0){
+      delay(500);
+      secs_2--;
+      display2_on();
+      }
+    }
+  } 
+
+
+
+  if(mode_counter==9)
+  {
+    display2_increment_on();
+    if(digitalRead(INCREMENT_BTN)==HIGH)
+    {
+      delay(500);
+      increment_2_min++;
+      display2_increment_on();
+    }
+    if(digitalRead(DECREMENT_BTN)==HIGH)
+    {
+      if(increment_2_min != 0){
+      delay(500);
+      increment_2_min--;
+      display2_increment_on();
+      }
+    }
+  }
+
+
+  if(mode_counter==10)
+  {
+    display2_increment_on();
+    if(digitalRead(INCREMENT_BTN)==HIGH)
+    {
+      delay(500);
+      increment_2_sec++;
+      display2_increment_on();
+    }
+    if(digitalRead(DECREMENT_BTN)==HIGH)
+    {
+      if(increment_2_sec != 0){
+      delay(500);
+      increment_2_sec--;
+      display2_increment_on();
+      }
+    }
+  }
+
+  if (mode_counter >10)
+  {
+    display2_on();
+    mode_counter = 0 ;
+    return;
+  }
+
+
+
+
+
+
+
+
+
+
+
   }
 }
-
-
 void display1_init(void){
   if (!display1.begin(SSD1306_SWITCHCAPVCC, SCREEN_1_ADDRESS)) {
     Serial.println(F("SSD1306 display 1 allocation failed"));
@@ -155,16 +319,23 @@ void display2_init(void){
     for (;;);
   }
 }
-void display1_on(void)
-{
+void display1_on(void){
 
   sprintf(buffer1, "%d: %d: %d",hours_1,mins_1,secs_1);
   display1.clearDisplay();
   display1.setTextSize(2);
   display1.setTextColor(WHITE);
-  display1.setCursor(15, 20);
+  display1.setCursor(20, 30);
   display1.println(buffer1);
   display1.display();  
+
+  // HEADING
+
+  display1.setTextSize(1);
+  display1.setTextColor(WHITE);
+  display1.setCursor(45,5);
+  display1.println("TIMER 1");
+  display1.display();
  
 }
 void timer1_on(void){
@@ -217,12 +388,20 @@ void timer2_on(void){
 }
 void display2_on(void)
 {
-  sprintf(buffer2, "%d: %d: %d",hours_1,mins_1,secs_1);
+  sprintf(buffer2, "%d: %d: %d",hours_2,mins_2,secs_2);
   display2.clearDisplay();
   display2.setTextSize(2);
   display2.setTextColor(WHITE);
-  display2.setCursor(15, 20);
+  display2.setCursor(20, 30);
   display2.println(buffer2);
+  display2.display();  
+
+  // HEADING
+
+  display2.setTextSize(1);
+  display2.setTextColor(WHITE);
+  display2.setCursor(45,5);
+  display2.println("TIMER 2");
   display2.display();
 }
 void display1_off(void){
@@ -242,4 +421,35 @@ int set_initial_state(){
   else{
     return PLAYER_2;
   }
+}
+void display1_increment_on(void){
+  sprintf(buffer3, "X: %d: %d",increment_1_min,increment_1_sec);
+  display1.clearDisplay();
+  display1.setTextSize(2);
+  display1.setTextColor(WHITE);
+  display1.setCursor(20, 30);
+  display1.println(buffer3);
+  display1.display();  
+  // HEADING
+  display1.setTextSize(1);
+  display1.setTextColor(WHITE);
+  display1.setCursor(45,5);
+  display1.println("INCREMENT");
+  display1.display();
+}
+
+void display2_increment_on(void){
+  sprintf(buffer4, "X: %d: %d",increment_2_min,increment_2_sec);
+  display2.clearDisplay();
+  display2.setTextSize(2);
+  display2.setTextColor(WHITE);
+  display2.setCursor(20, 30);
+  display2.println(buffer4);
+  display2.display();  
+  // HEADING
+  display2.setTextSize(1);
+  display2.setTextColor(WHITE);
+  display2.setCursor(45,5);
+  display2.println("INCREMENT");
+  display2.display();
 }
